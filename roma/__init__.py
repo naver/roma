95cc5fec171abeb05206de6b3638878046818e41
Romain Brégier <romain.bregier@naverlabs.com>
2026-08-11 16:54:03 +0200

    Dynamic version number

--- roma/__init__.py
+++ roma/__init__.py
@@ -1,6 +1,14 @@
 # RoMa
 # Copyright (c) 2020 NAVER Corp.
 # 3-Clause BSD License.
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("roma")
+except PackageNotFoundError:
+    # Package is not installed (e.g. used as a git submodule).
+    __version__ = "unknown"
+
 from .mappings import *
 from .utils import *
 from .transforms import *
